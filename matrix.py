ca702b02de417a691c0bc783840ee2bd7a9bb6a1
Asami Doi <doiasami1219@gmail.com>
2017-06-07 14:38:35 +0900

    modify: correspond to python3

--- matrix.py
+++ matrix.py
@@ -1,12 +1,12 @@
 '''
-sample code
-https://github.com/xharaken/step2015/blob/master/matrix.py
+[sample code]
+(https://github.com/xharaken/step2015/blob/master/matrix.py)
 '''
 
 import numpy, sys, time
 
 if (len(sys.argv) != 2):
-    print "usage: python %s N" % sys.argv[0]
+    print("usage: python %s N" % sys.argv[0])
     quit()
 
 n = int(sys.argv[1])
@@ -15,8 +15,8 @@ b = numpy.zeros((n, n)) # Matrix B
 c = numpy.zeros((n, n)) # Matrix C
 
 # Initialize the matrices to some values.
-for i in xrange(n):
-    for j in xrange(n):
+for i in range(n):
+    for j in range(n):
         a[i, j] = i * n + j
         b[i, j] = j * n + i
         c[i, j] = 0
@@ -28,14 +28,14 @@ begin = time.time()
 ######################################
 
 end = time.time()
-print "time: %.6f sec" % (end - begin)
+print("time: %.6f sec" % (end - begin))
 
 # Print C for debugging. Comment out the print before measuring the execution time.
 total = 0
-for i in xrange(n):
-    for j in xrange(n):
+for i in range(n):
+    for j in range(n):
         # print c[i, j]
         total += c[i, j]
 # Print out the sum of all values in C.
 # This should be 450 for N=3, 3680 for N=4, and 18250 for N=5.
-print "sum: %.6f" % total
+print("sum: %.6f" % total)
